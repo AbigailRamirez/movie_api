b8422419f98c048c5a28e0e8848d087999b99b16
AbigailRamirez <ramirezabigail01@gmail.com>
2023-03-02 11:56:14 -0500

    created topMovies JSON

--- index.js
+++ index.js
@@ -1,6 +1,52 @@
+const { Console } = require('console');
 const express = require('express'),
     morgan = require('morgan');
 const app = express();
+
+//Top 10 movies JSON
+let topMovies = [
+    {
+      title: 'Castle in the Sky',
+      director: 'Hayao Miyazaki'
+    },
+    {
+      title: 'My Neighbor Totoro',
+      director: 'Hayao Miyazaki'
+    },
+    {
+      title: 'Kiki\'s Delivery Service',
+      director: 'Hayao Miyazaki'
+      },
+     {
+      title: 'Porco Rosso',
+      director: 'Hayao Miyazaki'
+      },
+      {
+      title: 'Princess Mononoke',
+      director: 'Hayao Miyazaki'
+      },
+       {
+      title: 'Spirited Away',
+      director: 'Hayao Miyazaki'
+      },
+         {
+      title: 'Howl\'s Moving Castle',
+      director: 'Hayao Miyazaki'
+      },
+      {
+      title: 'Ponyo',
+      director: 'Hayao Miyazaki'
+      },
+      {
+      title: 'Baby Driver',
+      director: 'Edgar Wright'
+      },
+      {
+      title: 'Shrek',
+      director: 'Vicky Jenson'
+      }    
+];
+
 
 app.use(express.static('public'));
 app.use(morgan('common'));
@@ -15,7 +61,9 @@ app.get('/documentation', (req, res) => {
     res.sendFile('public/documentation.html', { root: __dirname });
   });
   
+  //top movies page
 app.get('/movies', (req, res) => {
+    console.log('Top movies request')
     res.json(topMovies);
   });
   
